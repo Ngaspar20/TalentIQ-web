9516e2635b748f64ebd44170038d7d6ff085ef4d
Nuno <ngaspar10@gmail.com>
2026-07-08 20:47:48 +0200

    Add EmailBackend for case-insensitive email authentication

--- talentiq/settings.py
+++ talentiq/settings.py
@@ -109,6 +109,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTHENTICATION_BACKENDS = [
+    "accounts.backends.EmailBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
