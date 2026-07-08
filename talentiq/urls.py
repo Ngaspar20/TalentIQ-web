b0b9308de8b50d627df823560e07415827e35258
Nuno <ngaspar10@gmail.com>
2026-07-08 20:54:25 +0200

    Remove /setup/promote/ URL permanently

--- talentiq/urls.py
+++ talentiq/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from .dashboard import dashboard
 from ajuda.views import ajuda_view
-from .setup_view import promote_all
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("setup/promote/", promote_all, name="promote_all"),
     path("accounts/", include("accounts.urls")),
     path("vagas/", include("vagas.urls")),
     path("candidatos/", include("candidatos.urls")),
